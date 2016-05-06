949a65e4ed2a6cfbed27f31fcbaedf58a3f073ea
Jeneva Cronin <jcronin@cs.washington.edu>
2016-05-06 11:44:28 -0700

    Updated Process_ApertureData to include chance calculations of the open, closed, and in response times (the regular response times). Also updated aper_responseTime.m to include response times for within the target region to any changes in stimulation. When I was updating aper_responseTime.m I noticed that the responseTime_closed was written incorrectly (not sure if this was the version that was used for the paper though). In that closed section, some of the closed response times were overwritten by open response times.

--- aper_responseTime.m
+++ aper_responseTime.m
@@ -1,10 +1,12 @@
-function [ responseTime_open, responseTime_closed  ] = aper_responseTime(locs_movement, stimChanges, stimPeaks, Stm1, Fs, closedStimAmp, correctOnly)
+function [ responseTime_open, responseTime_closed, responseTime_in  ] = aper_responseTime(locs_movement, stimChanges, stimPeaks, Fs, targetStimAmp, closedStimAmp, correctOnly)
 % aper_responseTime returns the response timing in miliseconds of the subject, defined
 % as the time between the onset of an error signal stim (i.e., the
 % beginning of a stimulus train that indicates that the subject is outside
 % of the target region) and the subject changing the direction of motion of
-% their hand. If the correctOnly variable = 1, then this will only return
+% their hand. 
+% If the correctOnly variable = 1, then this will only return
 % response times of responses that were made in the correct direction.
+% This correctOnly portion has NOT BEEN CODED
 
 % If stim changes are outside of the boundary (i.e. stimPeaks are equal to
 % 0:too open, or the high intensity stim: too closed), count the samples
@@ -34,7 +36,21 @@ for i=1:length(stimChanges_closed)
         responseTime_closed(i) = locs_movement(index) - stimChanges_closed(i);
     catch
         warning('No significant movement changes after the stim state change');
-        responseTime_open = responseTime_closed(1:i-1);
+        responseTime_closed = responseTime_closed(1:i-1);
+        break
+    end
+end
+
+% Now, inside of boundary:
+stimChanges_in = stimChanges(stimPeaks(1:end-1)==targetStimAmp);
+responseTime_in = zeros(size(stimChanges_in));
+for i=1:length(stimChanges_in)
+    try
+        index = find(locs_movement>stimChanges_in(i), 1);
+        responseTime_in(i) = locs_movement(index) - stimChanges_in(i);
+    catch
+        warning('No significant movement changes after the stim state change');
+        responseTime_in = responseTime_in(1:i-1);
         break
     end
 end
@@ -42,6 +58,7 @@ end
 % Convert from samples to ms
 responseTime_open = responseTime_open/Fs*100;
 responseTime_closed = responseTime_closed/Fs*100;
+responseTime_in = responseTime_in/Fs*100;
     
 end
 

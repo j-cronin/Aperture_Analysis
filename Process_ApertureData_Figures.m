450cc9cb84c8cfd1dcd2dfcf819b4e5eb2b729c7
Jeneva Cronin <jcronin@cs.washington.edu>
2016-05-21 22:06:51 -0700

    Added to the Process_AperData_Figures to make the path/stim figures for the paper.

--- Process_ApertureData_Figures.m
+++ Process_ApertureData_Figures.m
@@ -1,4 +1,5 @@
-%% Plot results per subject with histograms (ksdensity is used at end)
+%% This plots results per subject with histograms (ksdensity is used at end)
+% Section below will plot the subject traces
 
 clear all, close all
 sids = ['ecb43e'; 'fca96e'; 'cdceeb'];
@@ -190,38 +191,38 @@ for ii=2:2
             'support','positive', 'function', 'pdf', 'bandwidth', width);
         plot(xi,f, 'LineWidth', 2)
         
-%         [f, xi] = ksdensity(cell2mat(responseTimeTrains_open(T)),...
-%             'support','positive', 'function', 'pdf', 'bandwidth', width);
-%         plot(xi,f)
-%         
-%         [f, xi] = ksdensity(cell2mat(responseTimeTrains_closed(T)),...
-%             'support','positive', 'function', 'pdf', 'bandwidth', width);
-%         plot(xi,f)
-%         
-%         [f, xi] = ksdensity(cell2mat(responseTimeTrains_in_NoCatch(T(~CT))),...
-%             'support','positive', 'function', 'pdf', 'bandwidth', width);
-%         plot(xi,f)
-%         
-%         % Response times to catch trial, if there was one
-%         if catchTrial % catchTrail is 0 if there wasn't one
-%             [f, xi] = ksdensity(responseTimeTrains_in{catchTrial},...
-%                 'support','positive', 'function', 'pdf', 'bandwidth', width);
-%             plot(xi,f)
-%             legend('chance response times, all', 'stim state changes, open',...
-%                 'stim state changes, closed', 'stim state changes, in',...
-%                 'train starts, open', 'train starts, closed', 'train starts, in', 'catch trial')
-%         else % set legend
-%             legend('chance response times, all', 'stim state changes, open',...
-%                 'stim state changes, closed', 'stim state changes, in',...
-%                 'train starts, open', 'train starts, closed', 'train starts, in')
-%         end
+        %         [f, xi] = ksdensity(cell2mat(responseTimeTrains_open(T)),...
+        %             'support','positive', 'function', 'pdf', 'bandwidth', width);
+        %         plot(xi,f)
+        %
+        %         [f, xi] = ksdensity(cell2mat(responseTimeTrains_closed(T)),...
+        %             'support','positive', 'function', 'pdf', 'bandwidth', width);
+        %         plot(xi,f)
+        %
+        %         [f, xi] = ksdensity(cell2mat(responseTimeTrains_in_NoCatch(T(~CT))),...
+        %             'support','positive', 'function', 'pdf', 'bandwidth', width);
+        %         plot(xi,f)
+        %
+        %         % Response times to catch trial, if there was one
+        %         if catchTrial % catchTrail is 0 if there wasn't one
+        %             [f, xi] = ksdensity(responseTimeTrains_in{catchTrial},...
+        %                 'support','positive', 'function', 'pdf', 'bandwidth', width);
+        %             plot(xi,f)
+        %             legend('chance response times, all', 'stim state changes, open',...
+        %                 'stim state changes, closed', 'stim state changes, in',...
+        %                 'train starts, open', 'train starts, closed', 'train starts, in', 'catch trial')
+        %         else % set legend
+        %             legend('chance response times, all', 'stim state changes, open',...
+        %                 'stim state changes, closed', 'stim state changes, in',...
+        %                 'train starts, open', 'train starts, closed', 'train starts, in')
+        %         end
         legend('chance response times, all', 'stim state changes, open',...
-                'stim state changes, closed', 'stim state changes, in')
-            
+            'stim state changes, closed', 'stim state changes, in')
+        
         xlim([0 500])
         title(['Response times: ', sid, ' ', type, sprintf('\nBandwidth = '), num2str(width)])
         
-        %% Response times but plotted with smoothed kernel rather than histograms, allowing algorithm to choose bandwidth     
+        %% Response times but plotted with smoothed kernel rather than histograms, allowing algorithm to choose bandwidth
         [f, xi, bw(1)] = ksdensity(cell2mat(reshape(chanceResponseTimes(T,:,:), [], 1)),...
             'support','positive', 'function', 'pdf');
         figure, plot(xi,f)
@@ -239,37 +240,110 @@ for ii=2:2
             'support','positive', 'function', 'pdf');
         plot(xi,f, 'LineWidth', 2)
         
-%         [f, xi, bw(5)] = ksdensity(cell2mat(responseTimeTrains_open(T)),...
-%             'support','positive', 'function', 'pdf');
-%         plot(xi,f)
-%         
-%         [f, xi, bw(6)] = ksdensity(cell2mat(responseTimeTrains_closed(T)),...
-%             'support','positive', 'function', 'pdf');
-%         plot(xi,f)
-%         
-%         [f, xi, bw(7)] = ksdensity(cell2mat(responseTimeTrains_in_NoCatch(T(~CT))),...
-%             'support','positive', 'function', 'pdf');
-%         plot(xi,f)
-%         
-%         % Response times to catch trial, if there was one
-%         if catchTrial % catchTrail is 0 if there wasn't one
-%             [f, xi, bw(8)] = ksdensity(responseTimeTrains_in{catchTrial},...
-%                 'support','positive', 'function', 'pdf');
-%             plot(xi,f)
-%             legend('chance response times, all', 'stim state changes, open',...
-%                 'stim state changes, closed', 'stim state changes, in',...
-%                 'train starts, open', 'train starts, closed', 'train starts, in', 'catch trial')
-%         else % set legend
-%             legend('chance response times, all', 'stim state changes, open',...
-%                 'stim state changes, closed', 'stim state changes, in',...
-%                 'train starts, open', 'train starts, closed', 'train starts, in')
-%         end
+        %         [f, xi, bw(5)] = ksdensity(cell2mat(responseTimeTrains_open(T)),...
+        %             'support','positive', 'function', 'pdf');
+        %         plot(xi,f)
+        %
+        %         [f, xi, bw(6)] = ksdensity(cell2mat(responseTimeTrains_closed(T)),...
+        %             'support','positive', 'function', 'pdf');
+        %         plot(xi,f)
+        %
+        %         [f, xi, bw(7)] = ksdensity(cell2mat(responseTimeTrains_in_NoCatch(T(~CT))),...
+        %             'support','positive', 'function', 'pdf');
+        %         plot(xi,f)
+        %
+        %         % Response times to catch trial, if there was one
+        %         if catchTrial % catchTrail is 0 if there wasn't one
+        %             [f, xi, bw(8)] = ksdensity(responseTimeTrains_in{catchTrial},...
+        %                 'support','positive', 'function', 'pdf');
+        %             plot(xi,f)
+        %             legend('chance response times, all', 'stim state changes, open',...
+        %                 'stim state changes, closed', 'stim state changes, in',...
+        %                 'train starts, open', 'train starts, closed', 'train starts, in', 'catch trial')
+        %         else % set legend
+        %             legend('chance response times, all', 'stim state changes, open',...
+        %                 'stim state changes, closed', 'stim state changes, in',...
+        %                 'train starts, open', 'train starts, closed', 'train starts, in')
+        %         end
         
         legend('chance response times, all', 'stim state changes, open',...
-                'stim state changes, closed', 'stim state changes, in')
-            
+            'stim state changes, closed', 'stim state changes, in')
+        
         xlim([0 500])
         title(['Response times: ', sid, ' ', type, sprintf('\nBandwidth = choosen for each fxn by ksdensity\n'), num2str(bw)])
         
     end
+end
+
+%% This will plot the subject traces
+sids = ['ecb43e'; 'fca96e'; 'cdceeb'];
+for ii=1:3
+    sid = sids(ii,:);
+    load(strcat('C:\Users\jcronin\Box Sync\Lab\ECoG\Aperture\Data Analysis\051016, for reviews\', sid))
+    figs =[9];
+    h=figure('position', [0, 0, 2070, 1059]);
+    %%
+    count=1;
+    for i = 1:length(figs)
+        switch(sid)
+            case 'fca96e'
+                trials = [2 5 7 8 9 10 12 13 15 16 17];
+                load(strcat('C:\Users\jcronin\Data\Subjects\fca96e\data\d7\fca96e_Aperture\Matlab\','Aperture-',num2str(trials(figs(i)))));
+            case 'ecb43e'
+                trials = [4 5 6 8 9 10 11 12 13 14 15 16 18 19];
+                load(strcat('C:\Users\jcronin\Data\Subjects\ecb43e\d7\Aperture_ecb43e\Matlab_Aperture\','Aperture_ecb43e-',num2str(trials(figs(i)))));
+            case 'cdceeb'
+                trials = [1 2 3];
+                load(strcat('C:\Users\jcronin\Data\Subjects\cdceeb(wrong-f3e511)\data\d14\Aperture_cdceeb\Matlab\','ApertureCheck-',num2str(trials(figs(i)))));
+        end
+        
+        fs_aper = Aper.info.SamplingRateHz; % sampling rate of the stim data (Hz)
+        fs_stim = Stim.info.SamplingRateHz;
+        startSampAper = floor(startTime(figs(i))*fs_aper);
+        endSampAper = floor(endTime(figs(i))*fs_aper);
+        startSampStim = floor(startTime(figs(i))*fs_stim);
+        endSampStim = floor(endTime(figs(i))*fs_stim);
+        
+        data = Aper.data(startSampAper:endSampAper,1);
+        high_boundary = Aper.data(startSampAper:endSampAper,2);
+        low_boundary = Aper.data(startSampAper:endSampAper,3);
+        
+        stimI = Stim.data(startSampStim:endSampStim, 4); % This is the stimulation current waveform that was set
+        stimV= Stim.data(startSampStim:endSampStim, 5); % This is the stimulation voltage as monitored during delivery at the IZ
+        % Times:
+        t_aper = (0:1:length(data)-1)/fs_aper; % This was defined previously over the entire time range for use in the aper_startANDendTimes function; now redefining over just the period of interest
+        t_stim = (0:length(stimI)-1)/fs_stim; % stim timing (s), starting from the starting Stim (armed) sample)
+        
+        
+        %h.Units = 'inches';
+        subplot(length(figs)*3,1,[count count+1])
+        ax=gca;
+        plot(t_aper, data, 'k', 'Linewidth', 2), hold on
+        plot(t_aper, high_boundary,'k:', 'Linewidth', 1), 
+        plot(t_aper, low_boundary,'k:', 'Linewidth', 1)
+        title(['Trial ' num2str(figs(i))]),
+        legend('Aperture (position)', 'High/Low thresholds')
+        ylabel('Hand aperture position')
+        ax.XTickLabel=[];
+        ax.XMinorTick='on';
+        set(ax, 'box', 'off');
+        ax.YLim=[min(data)-0.1 max(data)+0.1];
+        set(ax, 'Ytick', 0:0.5:1);
+        set(gca, 'XLim',[0 130])
+        
+        subplot(length(figs)*3,1, count+2)
+        ax=gca;
+        plot(t_stim, stimI*2/1000, 'k')
+        xlabel('Trial time (s)')
+        legend('Biphasic stim trains')
+       
+        ax.YLim=[-4 4];
+        ax.XMinorTick='on';
+        ylabel('Current (mA)')
+        set(ax, 'box', 'off');
+        set(gca, 'XLim',[0 130])
+        
+        count=count+3;
+    end
+    %%
 end
